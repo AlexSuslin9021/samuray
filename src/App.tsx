56b0754dc0856be30a165020beab9a8f50a1102c
Alexsus <alexsuslin@inbox.ru>
2022-12-26 15:07:47 +0300

    Change components Header and Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header/>
+        <Technologies/>
     </div>
   );
+}
+function Header(){
+    return <div>
+        <a href={'#s'}>Home </a>
+        <a href={'#s'}>Message </a>
+        <a href={'#s'}> Get </a>
+    </div>
+}
+function Technologies(){
+    return <div>
+       <ul>
+           <li>HTML</li>
+           <li>CSS</li>
+           <li>JS</li>
+           <li>React</li>
+       </ul>
+    </div>
 }
 
 export default App;

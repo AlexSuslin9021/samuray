4716c51ae9c2a49e71e75f823aa9ddddd9e713b4
Alexsus <alexsuslin@inbox.ru>
2023-01-03 16:57:53 +0300

    Add style for components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import s from './Header.module.css'
 export function Header() {
-    return <header className='header'>
+    return <header className={s.header}>
             <img src="https://i.pinimg.com/550x/99/f7/0f/99f70fe7d427e6c6cf994260ff04f24b.jpg" alt=""/>
         </header>
 
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import a from './Profile.module.css'
 
 export function Profile(){
     return <div className='contains'>
-        <div>
+        <div className={a.item}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlCzLDRdu5WjTxrUB5Sd4bN022QuUH5ofeBf7w02xNYSHIAnkhttKQiqzBxlLpoJKuRTQ&usqp=CAU" alt=""/>
         </div>
-        <div>
+        <div className={a.item}>
             ava + descript
         </div>
-        <div> My post
-            <div>
+        <div className={a.item}> My post
+            <div className={a.item}>
                 New post
             </div>
         </div>
         <div>
-            <div>
+            <div className={a.item}>
                 post 1
             </div>
-            <div>
+            <div className={a.item}>
                 post 2
             </div>
         </div>

11c00f6776869e29a17786f97800f5375417b097
Alexsus <alexsuslin@inbox.ru>
2023-01-10 16:50:01 +0300

    Add  directiry ProfileInfo  and create coponent PrifileInfo

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,22 +2,45 @@
 import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+
+type DialogsItemProps ={
+    name:string
+    to:string
+}
+const DialogsItem=(props:DialogsItemProps)=>{
+    return <div className={s.user}><NavLink to={props.to}> {props.name}</NavLink></div>
+}
+type DialogProps={
+    dial:string
+}
+const Dialog=(props:DialogProps)=>{
+    return<div className={s.dialog}> {props.dial}</div>
+}
 function Dialogs(){
     return<div className={s.dialogsContent}>
         <div className={s.users}>
-        <div className={s.user}>Alex</div>
-            <div className={s.user}><NavLink to={'/dialogs/1'}> Alex</NavLink></div>
-            <div className={s.user}><NavLink to={'/dialogs/2'}>Dima</NavLink></div>
-            <div className={s.user}><NavLink to={'/dialogs/3'}>Tanua</NavLink></div>
-            <div className={s.user}><NavLink to={'/dialogs/4'}>Misha</NavLink></div>
-            <div className={s.user}><NavLink to={'/dialogs/5'}>Serg</NavLink></div>
-            <div className={s.user}><NavLink to={'/dialogs/6'}>Valera</NavLink></div>
+        {/*<div className={s.user}>Alex</div>*/}
+            {/*<div className={s.user}><NavLink to={'/dialogs/1'}> Alex</NavLink></div>*/}
+            <DialogsItem to={'/dialogs/1'} name ='Alex'/>
+            <DialogsItem to={'/dialogs/2'} name ='Dima'/>
+            <DialogsItem to={'/dialogs/3'} name ='Misha'/>
+            <DialogsItem to={'/dialogs/4'} name ='Tanya'/>
+            <DialogsItem to={'/dialogs/5'} name ='Valera'/>
+            <DialogsItem to={'/dialogs/6'} name ='Serg'/>
+            {/*<div className={s.user}><NavLink to={'/dialogs/2'}>Dima</NavLink></div>*/}
+            {/*<div className={s.user}><NavLink to={'/dialogs/3'}>Tanua</NavLink></div>*/}
+            {/*<div className={s.user}><NavLink to={'/dialogs/4'}>Misha</NavLink></div>*/}
+            {/*<div className={s.user}><NavLink to={'/dialogs/5'}>Serg</NavLink></div>*/}
+            {/*<div className={s.user}><NavLink to={'/dialogs/6'}>Valera</NavLink></div>*/}
         </div>
 
         <div className={s.dialogs}>
-  <div className={s.dialog}> Hello!</div>
-  <div className={s.dialog}> How are you? </div>
-  <div className={s.dialog}> I am good!</div>
+            <Dialog dial={'Hello'}/>
+            <Dialog dial={'How are you?'}/>
+            <Dialog dial={'I am good!'}/>
+  {/*<div className={s.dialog}> Hello!</div>*/}
+  {/*<div className={s.dialog}> How are you? </div>*/}
+  {/*<div className={s.dialog}> I am good!</div>*/}
     </div>
 
     </div>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,19 +2,13 @@ import React from "react";
 import a from './Profile.module.css'
 // import {MyPost} from './MyPost/MyPost'
 import {MyPost} from './MyPost/MyPost'
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 export function Profile(){
-    return <div className='contains'>
-        <div className={a.item}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlCzLDRdu5WjTxrUB5Sd4bN022QuUH5ofeBf7w02xNYSHIAnkhttKQiqzBxlLpoJKuRTQ&usqp=CAU" alt=""/>
+    return(
+        <div className='contains'>
+            <ProfileInfo/>
+            <MyPost/>
         </div>
-        <div className={a.item}>
-            ava + descript
-        </div>
-        <textarea ></textarea>
-        <button> Add</button>
-
-        <MyPost/>
-    </div>
-
+)
 }
 export default Profile

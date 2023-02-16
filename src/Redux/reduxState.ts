0e0bd43a38c0aeb0833a9a171cadf8cdc73c1df5
Alexsus <alexsuslin@inbox.ru>
2023-02-16 11:53:29 +0300

    create reduxState.ts 2

---
+++ src/Redux/reduxState.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import {combineReducers, createStore} from "redux";
+import reducerSidebar from "./reducerSidebar";
+import reducerProfile from "./reducerProfile";
+import reducerDialogs from "./reducerDialogs";
+
+let redusers = combineReducers({
+    reducerSidebar,
+    reducerProfile,
+    reducerDialogs
+}
+)
+let store = createStore(redusers)

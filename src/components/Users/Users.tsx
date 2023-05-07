ab76d6eadfd9def3e9877055933f302a616856ce
Alexsus <alexsuslin@inbox.ru>
2023-05-07 21:27:05 +0300

    - refactor paginator

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 import usersPhoto from '../../assets/image/3607444.png'
 import s from "./Users.module.css";
@@ -33,13 +33,24 @@ export const Users = (props: PropsUsersType) => {
     for (let i = 1; i <= pageCount; i++) {
         page.push(i)
     }
+const portionSize=10;
+    const portionCount=Math.ceil(props.totalUsersCount /portionSize) //сколько порций всего
+    let[portionNumber, setPortionNumber]=useState(1);
+    let leftPortionPageNumber=(portionNumber-1)*portionSize+1;
+    let rightPortionPageNumber=portionNumber*portionSize
+
+
+
 
     return (
         <div>
             {props.isFetching && <div><img src={preloader} alt=""/></div>}
-
-            {page.map((p, index) => <span key={index} className={props.currentPage === p ? s.selected : ''}
-                                          onClick={() => props.onClickPage(p)}>{p}</span>)}
+            {portionNumber > 1 && <button onClick={()=>setPortionNumber(portionNumber-1)}>Back</button>}
+            {page
+                .filter(p=>p >= leftPortionPageNumber && p<= rightPortionPageNumber)
+                .map((p, index) => <span key={index} className={props.currentPage === p ? s.selected : ''}
+                                          onClick={() => props.onClickPage(p)}>{p} </span>)}
+            {portionCount > portionNumber && <button onClick={()=>setPortionNumber(portionNumber+1)}>next</button>}
             {props.users.map(u => <div key={u.id}>
                 <div className={s.iconContainer}>
                     <NavLink to={'/profile/' + u.id}>

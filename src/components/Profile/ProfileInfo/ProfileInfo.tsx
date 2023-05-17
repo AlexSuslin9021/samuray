45dd99c26cf6e6e5449081e95a6913342d813c67
Alexsus <alexsuslin@inbox.ru>
2023-05-17 10:33:33 +0300

    - add information about me in Profile

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -31,12 +31,12 @@ export function ProfileInfo(props: ProfileTypeInfo) {
             <div className={s.description}>
                 <ProfileStatus status={props.status}  updateProfileStatus={props.updateProfileStatus}    />
                 <div className={s.item}></div>
-                <div> {props.profile.aboutMe}</div>
-                <div> {props.profile.fullName}</div>
-                <div> Date of Birth</div>
-                <div> Location</div>
-                <div> Education</div>
-                <div> {props.profile.contacts.website}</div>
+                <div> <b>About me:</b> {props.profile.aboutMe}</div>
+                <div> <b>Full name:</b> {props.profile.fullName}</div>
+                <div> <b> Date of Birth:</b></div>
+                <div><b> Location:</b></div>
+                <div> <b> Education:</b></div>
+                <div> {Object.keys(props.profile.contacts).map(c=><ul><li><b>{c}:</b></li></ul>) }</div>
 
             </div>
 

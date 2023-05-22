cc2434f9d9e072ac6db33903e50962ea6039a4fc
Alexsus <alexsuslin@inbox.ru>
2023-05-22 22:00:10 +0300

    - add handle-error in authReducer

--- src/Redux/authReducers/authReducer.ts
+++ src/Redux/authReducers/authReducer.ts
@@ -2,8 +2,8 @@ import {authApi, captchaApi, usersApi} from "../../API/api";
 import {Dispatch} from "redux";
 import {ThunkAction} from "redux-thunk";
 import {stopSubmit} from 'redux-form'
-import {handleServerNetworkError} from "../../common/error-utils/error-utils";
-import {AxiosResponse} from "axios";
+import {handleServerAppError, handleServerNetworkError} from "../../common/error-utils/error-utils";
+import axios, {AxiosResponse} from "axios";
 
 let initialState: initialStateType = {
     data: {id: '', login: "", email: "@inbox.ru"},
@@ -11,18 +11,18 @@ let initialState: initialStateType = {
     fieldsErrors: [],
     resultCode: 0,
     isAuth: false,
-    captcha:null
+    captcha: null
 }
 
 export const authReducer = (state: initialStateType = initialState, action: ActionType): initialStateType => {
     switch (action.type) {
         case setUserData:
-        return {...state, data: {...action.data}}
+            return {...state, data: {...action.data}}
         case "CHANGE_ISAUTH":
-            return {...state, isAuth:action.isAuth}
+            return {...state, isAuth: action.isAuth}
         case "GET-CAPTCHA":
             debugger
-            return {...state, captcha:action.captcha}
+            return {...state, captcha: action.captcha}
         default:
             return state
     }
@@ -30,59 +30,65 @@ export const authReducer = (state: initialStateType = initialState, action: Acti
 
 export const setUserDataAC = (data: DataType) => {return {type: setUserData, data} as const}
 export const isAuthAC = (isAuth: boolean) => {return {type: 'CHANGE_ISAUTH', isAuth} as const}
-export const getCaptchaAC=(captcha:string) =>{return{type:'GET-CAPTCHA', captcha} as const }
-export const setUserThunkCreator = (id: string | null , email: string | null, login: string | null) => ({
+export const getCaptchaAC = (captcha: string) => {return {type: 'GET-CAPTCHA', captcha} as const}
+
+export const setUserThunkCreator = (id: string | null, email: string | null, login: string | null) => ({
     type: 'SET_USER_DATA', data: {id, email, login}
 }) as const
-
 export const getAuthThunkCreator = (): ThunkAction<Promise<void>, initialStateType, unknown, ActionType> => {
-   return   async (dispatch: Dispatch<ActionType>) => {
-       try {
-           const response=await authApi.me()
-           if (response.data.resultCode === 0) {
-               let {id, login, email} = response.data.data
-               dispatch(setUserDataAC({id, email, login}))
-               dispatch(isAuthAC(true))
-           }
-       } catch (e:any){
-           handleServerNetworkError(e.message,dispatch)
-       }
-
-            // dispatch(setUserDataAC(response.data))
-
+    return async (dispatch: Dispatch<ActionType>) => {
+        try {
+            const response = await authApi.me()
+            if (response.data.resultCode === 0) {
+                let {id, login, email} = response.data.data
+                dispatch(setUserDataAC({id, email, login}))
+                dispatch(isAuthAC(true))
+            }
+        } catch (e: any) {
+            handleServerNetworkError(e.message, dispatch)
+        }
     }
-
 }
-export const loginThunkCreator = (email: string, password: string, rememberMe: boolean = false, captcha?:string|null): ThunkAction<Promise<void>, any, unknown, ActionType> => {
-debugger
+export const loginThunkCreator = (email: string, password: string, rememberMe: boolean = false, captcha?: string | null): ThunkAction<Promise<void>, any, unknown, ActionType> => {
+    debugger
     return async (dispatch: any) => {
-      let response=await  authApi.loginCreate(email, password, rememberMe,captcha)
+        try {
+            let response = await authApi.loginCreate(email, password, rememberMe, captcha)
             if (response.data.resultCode === 0) {
                 dispatch(getAuthThunkCreator())
             } else {
-                if(response.data.resultCode===10){
+                if (response.data.resultCode === 10) {
                     dispatch(getCaptchaTC())
+                }else {
+                    handleServerAppError(response.data, dispatch)
                 }
-                let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
-                dispatch(stopSubmit("Login", {_error: message}))
             }
+        }catch (e) {
+            if (axios.isAxiosError(e))
+                handleServerNetworkError(e, dispatch)
+        }
     }
 }
 export const loginOutThunkCreator = (): ThunkAction<Promise<void>, initialStateType, unknown, ActionType> => {
     return async (dispatch: any) => {
-       await authApi.loginDelete().then(response => {
+        try {
+            let response = await authApi.loginDelete()
             if (response.data.resultCode === 0) {
-                dispatch(setUserThunkCreator(null, null, null, ))
+                dispatch(setUserThunkCreator(null, null, null,))
                 dispatch(isAuthAC(false))
+            } else {
+                handleServerAppError(response.data, dispatch)
             }
-            // dispatch(setUserDataAC(response.data))
-        })
+        } catch (e) {
+            if (axios.isAxiosError(e))
+                handleServerNetworkError(e, dispatch)
+        }
     }
 }
 export const getCaptchaTC = (): ThunkAction<Promise<void>, initialStateType, unknown, ActionType> => {
     debugger
     return async (dispatch) => {
-     let response = await captchaApi.getCaptcha();
+        let response = await captchaApi.getCaptcha();
         dispatch(getCaptchaAC(response.data.url))
 
 
@@ -98,12 +104,12 @@ export type initialStateType = {
     fieldsErrors: [],
     resultCode: number
     isAuth: boolean
-    captcha:null |string
+    captcha: null | string
 }
 export type DataType = {
     id: string | null
     email: string | null,
     login: string | null
-    captcha?:null |string
+    captcha?: null | string
 }
 type ActionType = ReturnType<typeof setUserDataAC> | ReturnType<typeof isAuthAC> | ReturnType<typeof getCaptchaAC>
